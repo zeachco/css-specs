d6c5a7e05129a554995ba2b125ecea397ef8481c
Olivier <zeachco@gmail.com>
2016-01-06 14:31:09 -0500

    clean

--- index.js
+++ index.js
@@ -9,33 +9,6 @@ var theme = 'adp';
 var specs = {
   'h1': ['font-size', 'display']
 };
-
-function phantomscript() { // phantom context
-  // document.querySelector()
-  console.log(JSON.stringify(window.spec));
-  var snap = {};
-  for (var n in window.spec) {
-    var node = document.querySelector(n);
-    var rules = window.spec[n];
-    var computed = getComputedStyle(node);
-    snap[n] = {};
-    for (var i = 0; i < rules.length; i++) {
-      var rule = rules[i];
-      snap[n][rule] = computed[rule];
-    }
-  }
-  return snap;
-}
-
-function nodescript(val) {
-  logger(val, 'blue');
-}
-
-$.visit('http://127.0.0.1:5000/compare?theme=' + cssFile, function() {
-  $.getPage(function(page) {
-    $('body').append('<script type="text/javascript">window.spec = ' + JSON.stringify(specs) + '</script>', function() {
-
-    });
-    page.evaluate(phantomscript, nodescript);
-  });
+renderer('http://perdu.com', specs, function(snapshot) {
+  logger(JSON.stringify(snapshot), 'blue');
 });
