4865ad1dec3588351cadc6cccd8d9e65d53c501b
Olivier <zeachco@gmail.com>
2016-01-07 12:02:43 -0500

    allowed usage from node require('css-specs')

--- index.js
+++ index.js
@@ -2,25 +2,40 @@ var path = require('path');
 var renderer = require('./lib/renderer');
 var server = require('./lib/server');
 var comparator = require('./lib/comparator');
-var logger = require('./lib/logger')('mainController');
+var logger = require('./lib/logger')('css-specs');
 var snapshot = require('./lib/snapshot');
 
-server.start();
-
 var config = require('./config');
-
 var theme = 'appdirect';
-
 var specs = require(path.join(process.cwd(), config.specsPath))
 
-renderer('http://localhost:' + config.serverPort + '/compare?theme=' + theme, specs, function(snap) {
-  // snapshot.save(JSON.stringify(snap));
-  var diff = comparator.compare(snap)
-  if (Object.keys(diff).length === 0) {
-    logger('✔ no diff detected', 'green');
-  } else {
-    logger(diff, 'red');
-  }
-  logger(JSON.stringify(snap), 'blue');
+server.start();
+compare('http://localhost:' + config.serverPort + '/compare?theme=' + theme, specs, function() {
   server.stop();
 });
+
+function snapshot(url, stylesheet, specs, callback) {
+  logger('fetching <' + url + '> ...');
+  renderer(url, specs, function(snap) {
+    snapshot.save(JSON.stringify(snap));
+  });
+}
+
+function compare(url, stylesheet, specs, callback) {
+  logger('coparing <' + url + '> with last snapshot...');
+  renderer(url, specs, function(snap) {
+    var diff = comparator.compare(snap)
+    if (Object.keys(diff).length === 0) {
+      logger('✔ no diff detected', 'green');
+    } else {
+      logger(diff, 'red');
+    }
+    logger(JSON.stringify(snap), 'blue');
+  });
+}
+
+module.exports = {
+  snapshot: snapshot,
+  compare: compare,
+  server: server,
+};
