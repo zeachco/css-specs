b67b9971df4073fc41ec716a3c5b4461d367fce3
Olivier <zeachco@gmail.com>
2016-01-08 11:49:27 -0500

    removed watcher

--- example/index.js
+++ example/index.js
@@ -11,35 +11,19 @@ var url = './example.html';
 // cssBuildPath = '/home/olivier/somebuild.css';
 
 var cssToCheck = fs.readFileSync(cssBuildPath, 'utf-8');
-var rendererIsRunning = false;
 
 if (cs.utils.snapshotExist(cssBuildPath)) {
-  var compare = function() {
-    log('comparing...')
-    if (rendererIsRunning)
-      return
-    rendererIsRunning = true;
-    cs.renderer(url, cssToCheck, function(result) {
-      var validshot = require(cs.utils.snapshotPath(cssBuildPath));
-      var diff = cs.comparator.compare(validshot, result.styles);
-      cs.reporter(diff)
-      timer();
-      rendererIsRunning = false;
-      setTimeout(compare, 2000);
-    });
-  }
-
-  log('watching for ' + cssBuildPath);
-  fs.watch(cssBuildPath, compare);
-  compare();
-
-} else if (!rendererIsRunning) {
-  rendererIsRunning = true;
+  cs.renderer(url, cssToCheck, function(result) {
+    var validshot = require(cs.utils.snapshotPath(cssBuildPath));
+    var diff = cs.comparator.compare(validshot, result.styles);
+    cs.reporter(diff)
+    timer();
+  });
+} else {
   log('snapshot does not exist, creating it...', 'red');
   cs.renderer(url, cssToCheck, function(result) {
     cs.snapshot.save(cssBuildPath, result.styles);
     log('snapshot saved, you may run it again after changes');
     timer();
-    rendererIsRunning = false;
   });
 }
