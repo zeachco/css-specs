87517be51074ee1a55ebbae6e7975d78695951c2
Olivier <zeachco@gmail.com>
2016-01-07 20:14:51 -0500

    node usage and reorganisation

--- example/index.js
+++ example/index.js
@@ -1,20 +1,36 @@
 var cSpecs = require('..'); // that's our css-specs lib
 var fs = require('fs');
 
-var logger = require('../lib/logger')('example', 'yellow');
+var log = cSpecs.log('example', 'yellow');
 
-cSpecs.manual();
-
-var cssToCheck = fs.readFileSync('./build.min.css', 'utf-8');
+var cssBuildPath = './build.min.css';
+var cssToCheck = fs.readFileSync(cssBuildPath, 'utf-8');
 var specs = require('./specs.js');
+var url = 'http://perdu.com';
 
-cSpecs.renderer('http://perdu.com', cssToCheck, specs, function(result) {
-
-  // save snapshot
-  if (result.stats.stylesSize > 0) {
-    cSpecs.snapshot.save(result.styles);
-  } else {
-    logger('Nothing to save');
-  }
-
-});
+if (cSpecs.utils.snapshotExist(cssBuildPath)) {
+  log('Getting current styles...');
+  cSpecs.renderer(url, cssToCheck, specs, function(result) {
+    var validshot = require(cSpecs.utils.snapshotPath(cssBuildPath));
+    log('Comparing to last valid snapshot...');
+    cSpecs.comparator.compare(validshot, result.styles, function(diff) {
+      log(diff);
+      if (Object.keys(diff).length) {
+        logger('✔ no diff detected', 'green');
+      } else {
+        logger('✗ build doesn\'t match!', 'red');
+        logger(diff, 'red');
+      }
+    });
+  });
+} else {
+  log('snapshot does not exist, creating it...', 'red');
+  cSpecs.renderer(url, cssToCheck, specs, function(result) {
+    if (result.stats.stylesSize > 0) {
+      cSpecs.snapshot.save(cssBuildPath, result.styles);
+      log('snapshot saved, you may run it again after changes');
+    } else {
+      log('Nothing to save');
+    }
+  });
+}
--- index.js
+++ index.js
@@ -4,55 +4,59 @@ var server = require('./lib/server');
 var comparator = require('./lib/comparator');
 var logger = require('./lib/logger')('css-specs', 'blue');
 var snapshot = require('./lib/snapshot');
+var utils = require('./lib/utils');
 
 var config = require('./config');
 var ran = false;
 
-setTimeout(function() {
-  if (!ran) {
-    logger('loading config from <' + process.cwd() + '/css-specs.conf>');
-    var theme = 'appdirect';
-    var specs = require(path.join(process.cwd(), config.specsPath))
-    server.start();
-    compare('http://localhost:' + config.serverPort + '/compare?theme=' + theme, specs, server.stop);
-  } else {
-    logger('using node manual execution');
-  }
-}, 0);
+// setTimeout(function() {
+//   if (!ran) {
+//     logger('loading config from <' + process.cwd() + '/css-specs.conf>');
+//     var theme = 'appdirect';
+//     var specs = require(path.join(process.cwd(), config.specsPath))
+//     server.start();
+//     compare('http://localhost:' + config.serverPort + '/compare?theme=' + theme, specs, server.stop);
+//   } else {
+//     logger('using node manual execution');
+//   }
+// }, 0);
 
-function execSnapshot(url, specs, callback) {
-  ran = true;
-  logger('fetching <' + url + '> ...');
-  renderer(url, specs, function(snap) {
-    snapshot.save(JSON.stringify(snap));
-    if (typeof callback == 'function')
-      callback(snap);
-  });
-}
+// function execSnapshot(url, specs, callback) {
+//   ran = true;
+//   logger('fetching <' + url + '> ...');
+//   renderer(url, specs, function(snap) {
+//     snapshot.save(JSON.stringify(snap));
+//     if (typeof callback == 'function')
+//       callback(snap);
+//   });
+// }
 
-function compare(url, specs, callback) {
-  ran = true;
-  logger('comparing <' + url + '> with last snapshot...');
-  renderer(url, specs, function(snap) {
-    var diff = comparator.compare(snap)
-    if (Object.keys(diff).length === 0) {
-      logger('✔ no diff detected', 'green');
-    } else {
-      logger(diff, 'red');
-    }
-    logger(JSON.stringify(snap));
-    if (typeof callback == 'function')
-      callback(snap);
-  });
-}
+// function compare(url, css, specs, callback) {
+//   ran = true;
+//   logger('comparing <' + url + '> with last snapshot...');
+//   renderer(url, specs, function(snap) {
+//     var diff = comparator.compare(snap)
+//     if (Object.keys(diff).length === 0) {
+//       logger('✔ no diff detected', 'green');
+//     } else {
+//       logger('✗ diff detected', 'red');
+//       logger(diff, 'red');
+//     }
+//     logger(JSON.stringify(snap));
+//     if (typeof callback == 'function')
+//       callback(snap);
+//   });
+// }
 
 module.exports = {
   manual: function() {
     ran = true
   },
-  logger: logger,
+  log: require('./lib/logger'),
   renderer: renderer,
   snapshot: snapshot,
-  compare: compare,
+  // compare: compare,
+  comparator: comparator,
   server: server,
+  utils: utils,
 };
